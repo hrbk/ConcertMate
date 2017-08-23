c9c116c601b19169729634fbf6936bb6905feb0b
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-23 14:56:53 -0700

    comment out database queries, currently retrieves data based on time and location to songkick

--- server/songkick-router.js
+++ server/songkick-router.js
@@ -10,38 +10,49 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.post('/', (req, res) => {
-	let date = req.body.date;
+  let date = req.body.date;
   let lat = req.body.lat;
   let lng = req.body.lng;
-  db.getEvents(date, (dbEvents) => {
-    if (dbEvents.length) {
-        res.send(dbEvents);
-    } else {
+  console.log('ALL THE INFO:', date, lat, lng);
+  let url = `http://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=geo:${lat},${lng}&min_date=${date}&max_date=${date}`;
+
+  axios.get(url)
+  .then((events) => {
+    let data = events.data.resultsPage.results.event;
+    res.send(data);
+  });
+
+  // db.getEvents(date, (dbEvents) => {
+    // if (dbEvents.length) {
+    //     res.send(dbEvents);
+    // } else {
       // here you could add functionality to setting a min/max date to search between
-    	let url = `http://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=geo:${lat},${lng}&min_date=${date}&max_date=${date}`;
-      axios.get(url)
-        .then((events) => {
-          let data = events.data.resultsPage.results.event;
-          async.each(data, (event, callback) => {
-            db.createEvent(event);
-            console.log('event saved: ', event);
-            callback();
-          }, (err) => {
-            setTimeout(function() {
-              db.getEvents(date, (newEvents) => {
-                //new events is returning empty when it should have the newly saved event data, set timeout is a patch
-                console.log('getting events: ', newEvents);
-                res.send(newEvents);
-            }, 500);
-            });
-          });
-        });
-      }
-    })
-    .catch((err) => {
-      console.log('ERROR ', err);
-      res.status(404).send(); 
-    });
+      // let url = `http://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=geo:${lat},${lng}&min_date=${date}&max_date=${date}`;
+      // axios.get(url)
+      //   .then((events) => {
+      //     let data = events.data.resultsPage.results.event;
+          // async.each(data, (event, callback) => {
+          //   db.createEvent(event);
+          //   // console.log('event saved: ', event);
+          //   callback();
+          // },
+          // (err) => {
+          //   setTimeout(function() {
+          //     db.getEvents(date, (newEvents) => {
+          //       //new events is returning empty when it should have the newly saved event data, set timeout is a patch
+          //       console.log('getting events: ', newEvents);
+          //       res.send(newEvents);
+          //   },
+            // 500);
+            // });
+        //   });
+        // });
+      // }
+//     })
+//     .catch((err) => {
+//       console.log('ERROR ', err);
+//       res.status(404).send();
+//     });
 });
 
 module.exports = router;
