dc345173bce52496d3baf95efba353718bbe3f84
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-23 19:00:36 -0700

    Render markers dynamically

--- client/src/components/Markers.jsx
+++ client/src/components/Markers.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// 
-// import {markerStyle, markerStyleHover} from './MarkerStyles.js';
 
 export default class Markers extends React.Component {
   constructor(props) {
@@ -15,7 +13,7 @@ export default class Markers extends React.Component {
   render() {
     // if the marker itself is being hovered or if the name of the venue corresponding to the
     // marker is the same as the name passed from the concert component, change the style accordingly
-    // also, it's literally just a css circle. you could probably add an image or something 
+    // also, it's literally just a css circle. you could probably add an image or something
     let style = this.props.$hover || this.state.name === this.props.hovered ? markerStyleHover : markerStyle;
     return (
       <div style={style}></div>
@@ -39,7 +37,6 @@ const markerStyleHover = {
   height: 20,
   left: -10,
   right: -10,
-
   border: '5px solid #0000CD',
   borderRadius: 20,
   backgroundColow: 'white'
