71679f92e31daceaecebc327475c7e31cfec906d
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-26 10:20:05 -0700

    import Button from react-bootstrap and render a filter button

--- client/src/components/Concerts.jsx
+++ client/src/components/Concerts.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import axios from 'axios';
 import ConcertEntry from './ConcertEntry.jsx';
 import {ListGroup} from 'react-bootstrap';
-import {Panel} from 'react-bootstrap';
+import {Panel, Button} from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 const Concerts = (props) => {
 
   return (
-    <Panel collapsible defaultExpanded header="Nearby Concerts" bsStyle="info">
+
+    <Panel collapsible defaultExpanded header="Nearby Concerts">
+      <Panel><Button>Filter by Popularity</Button></Panel>
       <ListGroup fill>
         {props.events.map((event, i) => {
           return <ConcertEntry event={event} key={i} handleArtistClick={props.handleArtistClick}/>
         })}
       </ListGroup>
     </Panel>
-
   )
 }
 
