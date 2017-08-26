7063379225060cb52722a9c6076eccf1af909825
kiyeonpark <kiyeon.c.park@gmail.com>
2017-08-26 14:52:06 -0700

    Separate component render for tabs depending on results vs popularity

--- client/src/components/Concerts.jsx
+++ client/src/components/Concerts.jsx
@@ -2,19 +2,52 @@ import React from 'react';
 import axios from 'axios';
 import ConcertEntry from './ConcertEntry.jsx';
 import {ListGroup} from 'react-bootstrap';
-import {Panel, Button} from 'react-bootstrap';
+import {Panel, Tabs, Tab} from 'react-bootstrap';
 import { connect } from 'react-redux';
 
-const Concerts = (props) => {
-  return (
 
+const Concerts = (props) => {
+
+  const nearby = (
     <Panel collapsible defaultExpanded header={`Nearby Concerts`}>
       <ListGroup fill>
         {props.events.map((event, i) => {
-          return <ConcertEntry event={event} key={i} handleArtistClick={props.handleArtistClick}/>
+          return <ConcertEntry event={event} key={i} handleArtistClick={props.handleArtistClick} />
         })}
       </ListGroup>
     </Panel>
+  )
+  const popularityObj = (events) => {
+    props.events.sort((a, b) => {
+      if (a.popularity > b.popularity) {
+        return 1;
+      }
+      if (a.popularity < b.popularity) {
+        return -1;
+      }
+      return 0;
+    }).reverse();
+  }
+
+  const popular = (
+    <Panel collapsible defaultExpanded header={`Popular Concerts`}>
+      <ListGroup fill>
+        {popularityObj(props.events)}
+        {props.events.map((event, i) => {
+          return <ConcertEntry event={event} key={i} handleArtistClick={props.handleArtistClick} />
+        })}
+      </ListGroup>
+    </Panel>
+  )
+
+  return (
+    <div>
+      <Tabs defaultActiveKey={1}>
+        <Tab eventKey={1} title='Nearby'>{nearby}</Tab>
+        <Tab eventKey={2} title='Popular'>{popular}</Tab>
+      </Tabs>
+    </div>
+
   )
 }
 
